9fa9d5624cb8fd68e1332f85ff513bfc54bd865f
tetzng <ffffffffffffunk@gmail.com>
2019-11-01 19:33:15 +0900

    Update music_controller.rb

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,4 +1,13 @@
 class MusicController < ApplicationController
   def index
+    require 'rspotify'
+
+    RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
+    @artists = RSpotify::Artist.search('fromis_9')
+    @artist = @artists.first
+    @albums = @artist.albums
+    @album = @albums.first
+    @tracks = @album.tracks
+    @track = @tracks.first
   end
 end
