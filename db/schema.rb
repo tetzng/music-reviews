3dc31c6ea3cf6582523e8bd1287e7b2208f2cc74
tetzng <ffffffffffffunk@gmail.com>
2019-11-01 19:33:07 +0900

    generate migration music.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_150915) do
+ActiveRecord::Schema.define(version: 2019_11_01_034633) do
+
+  create_table "musics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
