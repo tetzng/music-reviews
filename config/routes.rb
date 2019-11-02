3cff86a7cbefcff684aae76670be1d8116a96b37
tetzng <ffffffffffffunk@gmail.com>
2019-11-02 22:16:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'music/index'
+  root 'music#index'
+  resources :music do
+    collection do
+      get :search
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
