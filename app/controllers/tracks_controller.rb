dfccc5073184afdbfbcfd97f43e49a31793aff1f
tetzng <ffffffffffffunk@gmail.com>
2019-11-08 05:11:16 +0900

    Update tracks_controller.rb

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,4 +1,7 @@
 class TracksController < ApplicationController
   def index
+    RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
+    @keyword = "Numb"
+    @tracks = RSpotify::Track.search(@keyword)
   end
 end
