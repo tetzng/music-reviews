80c37f7aa920b9c15e27ca94ea3ab22d67e4af02
tetzng <ffffffffffffunk@gmail.com>
2019-11-08 23:04:18 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  require 'rspotify'
+  RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
 end
