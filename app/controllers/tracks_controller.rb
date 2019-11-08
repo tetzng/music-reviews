6e907ff643d160e3856e55b2d4c55f0da51a3f38
tetzng <ffffffffffffunk@gmail.com>
2019-11-08 23:04:26 +0900

    Update tracks_controller.rb

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,7 +1,20 @@
 class TracksController < ApplicationController
   def index
-    RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
     @keyword = "Numb"
     @tracks = RSpotify::Track.search(@keyword)
+  end
+
+  def show
+    track = RSpotify::Track.find(params[:spotify_id])
+    @track = Track.find_or_initialize_by(spotify_id: params[:spotify_id])
+    if @track.new_record?
+      @track.update_attributes!(name: track.name,
+                                track_number: track.track_number,
+                                duration_ms: track.duration_ms,
+                                preview_url: track.preview_url
+                              )
+    end
+    @album = track.album
+    @artist = track.artists[0]
   end
 end
