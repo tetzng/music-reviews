b96bb30d5e6a9b49ed731f754cf4647b9871abe3
tetzng <ffffffffffffunk@gmail.com>
2019-11-08 23:04:15 +0900

    Update albums_controller.rb

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,9 @@
 class AlbumsController < ApplicationController
   def index
-    RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
     @keyword = "Rubber"
     @albums = RSpotify::Album.search(@keyword)
+  end
+  def show
+    @album = RSpotify::Album.find(params[:spotify_id])
   end
 end
