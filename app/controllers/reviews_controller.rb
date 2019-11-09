d8325c3d741fd46f39091e28feb3dda704d4f3aa
tetzng <ffffffffffffunk@gmail.com>
2019-11-09 18:15:51 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,13 +5,13 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @track = Track.find(params[:spotify_id])
+    @track = Track.find_by(spotify_id: params[:spotify_id])
     Review.create(create_params)
-    redirect_to controller: :products, action: :index
+    redirect_to track_path(@track.spotify_id)
   end
 
   private
   def create_params
-    params.require(:review).permit(:rate, :review)
+    params.permit(:review)
   end
 end
