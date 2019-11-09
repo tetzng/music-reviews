b3dc9a3aa4d87b05aeed4ec9ca876482f4ec1d9c
tetzng <ffffffffffffunk@gmail.com>
2019-11-10 06:18:56 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,11 @@ class ReviewsController < ApplicationController
   def create
     @track = Track.find_by(spotify_id: params[:spotify_id])
     Review.create(create_params)
-    redirect_to track_path(@track.spotify_id)
+    redirect_to track_path(params[:track_spotify_id])
   end
 
   private
   def create_params
-    params.permit(:review)
+    params.permit(:rate, :review).merge(track_id: params[:track_spotify_id], user_id: current_user.id)
   end
 end
