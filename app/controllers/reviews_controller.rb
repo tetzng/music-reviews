7f3b66373ad9ab83c0e625d367321dc895564089
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 22:51:43 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,24 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
   def new
     @review = Review.new
     @track = Track.find(params[:spotify_id])
   end
 
   def create
-    @track = Track.find_by(spotify_id: params[:spotify_id])
-    Review.create(create_params)
+    @review = Review.create(create_params)
     redirect_to track_path(params[:track_spotify_id])
   end
 
   private
   def create_params
     if user_signed_in?
-      params.permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id], user_id: current_user.id)
+      params.require(:review).permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id], user_id: current_user.id)
     else
-      params.permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id])
+      params.require(:review).permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id])
     end
   end
 end
