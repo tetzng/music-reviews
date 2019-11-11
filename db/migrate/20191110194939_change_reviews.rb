60c297c064caeaeb145886e72780dbf377c0aade
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 19:20:45 +0900

    Update 20191110194939_change_reviews.rb

--- db/migrate/20191110194939_change_reviews.rb
+++ db/migrate/20191110194939_change_reviews.rb
@@ -1,7 +1,4 @@
 class ChangeReviews < ActiveRecord::Migration[5.2]
   def change
-    remove_column :reviews, :track_id
-    add_column :reviews, :track_spotify_id, :bigint, index: true
-    add_foreign_key :reviews, :tracks, column: :track_spotify_id
   end
 end
