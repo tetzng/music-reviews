9a905f5d4496e46262157641ba0d2371da43874f
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 20:39:33 +0900

    Update 20191110194939_change_reviews.rb

--- db/migrate/20191110194939_change_reviews.rb
+++ db/migrate/20191110194939_change_reviews.rb
@@ -1,4 +1,6 @@
 class ChangeReviews < ActiveRecord::Migration[5.2]
   def change
+    add_reference :reviews, :track_spotify, type: :string, limit: 32
+    add_foreign_key :reviews, :tracks, column: :track_spotify_id, primary_key: :spotify_id
   end
 end
