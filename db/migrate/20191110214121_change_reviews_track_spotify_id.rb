9920d305a49d67c8ed8802e6862f52ed0bc96207
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 20:39:35 +0900

    Update 20191110214121_change_reviews_track_spotify_id.rb

--- db/migrate/20191110214121_change_reviews_track_spotify_id.rb
+++ db/migrate/20191110214121_change_reviews_track_spotify_id.rb
@@ -1,5 +1,6 @@
 class ChangeReviewsTrackSpotifyId < ActiveRecord::Migration[5.2]
   def change
-
+    change_column :artists, :spotify_id, :string
+    change_column :albums, :spotify_id, :string
   end
 end
