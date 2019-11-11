7b0586a62dca6bca81fa152f33c3e9798cdcbea0
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 19:20:55 +0900

    Update 20191107185749_create_tracks.rb

--- db/migrate/20191107185749_create_tracks.rb
+++ db/migrate/20191107185749_create_tracks.rb
@@ -1,6 +1,6 @@
 class CreateTracks < ActiveRecord::Migration[5.2]
   def change
-    create_table :tracks do |t|
+    create_table :tracks, primary_key: :spotify_id do |t|
       t.string :name
       t.integer :track_number
       t.integer :duration_ms
