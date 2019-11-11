64bb388b6b0325acc9ee3345e0ace40bc3de5059
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 19:20:48 +0900

    Update 20191108112844_add_column_to_tracks.rb

--- db/migrate/20191108112844_add_column_to_tracks.rb
+++ db/migrate/20191108112844_add_column_to_tracks.rb
@@ -1,5 +1,6 @@
 class AddColumnToTracks < ActiveRecord::Migration[5.2]
   def change
-    add_column :tracks, :spotify_id, :string
+    change_column :tracks, :spotify_id, :string
+    add_index :tracks, :spotify_id
   end
 end
