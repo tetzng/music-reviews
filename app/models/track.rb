4b61fd2d6ec57575a25be7e5f6ac0667c5cdd1bb
tetzng <ffffffffffffunk@gmail.com>
2019-11-12 02:21:42 +0900

    Update track.rb

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,8 @@
 class Track < ApplicationRecord
   has_many :reviews, foreign_key: "track_spotify_id", primary_key: "spotify_id", class_name: 'Review'
 
+  validates :name, presence: true
+
   def to_param
     spotify_id
   end
