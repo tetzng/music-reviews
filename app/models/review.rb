511ddd43623e3f06727c3608ab7d4fa22e37a1f3
tetzng <ffffffffffffunk@gmail.com>
2019-11-11 22:51:34 +0900

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :track
+  belongs_to :track, foreign_key: "track_spotify_id", primary_key: "spotify_id", class_name: 'Track'
   belongs_to :user, optional: true
 end
