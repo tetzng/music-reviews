4494d5e968169ccc15821015b9fbab303b366984
tetzng <ffffffffffffunk@gmail.com>
2019-11-13 03:24:39 +0900

    added blank lines before and after protected and private.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     added_attrs = [ :name, :email, :password, :password_confirmation, :avatar ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
@@ -12,6 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
   def auth_spotify
     require 'rspotify'
     RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret])
