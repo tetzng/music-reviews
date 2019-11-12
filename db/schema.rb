437b08b93cc2a9fbaabd5f6b98dfa15c02ed44b9
tetzng <ffffffffffffunk@gmail.com>
2019-11-13 03:38:01 +0900

    [Corrected] Layout/EmptyLinesAroundBlockBody

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_11_161933) do
-
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
