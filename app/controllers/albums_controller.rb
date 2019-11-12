94ecf8f260697876d0396926223601f2e4ee60bd
tetzng <ffffffffffffunk@gmail.com>
2019-11-13 03:23:22 +0900

    added empty lines between method definitions

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,6 +3,7 @@ class AlbumsController < ApplicationController
     @keyword = params[:keyword]
     @albums = RSpotify::Album.search(@keyword)
   end
+
   def show
     @album = RSpotify::Album.find(params[:spotify_id])
   end
