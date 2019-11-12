acdca1013b70e658f872e22d68434ad7a5fdfc8a
tetzng <ffffffffffffunk@gmail.com>
2019-11-13 04:16:56 +0900

    [Corrected] Style/SymbolArray

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ gem 'tzinfo-data'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'pry-rails'
 end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:name, :email, :password, :password_confirmation, :avatar]
+    added_attrs = %i[name email password password_confirmation avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
