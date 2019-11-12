04419744c7d516ea4c0de57b5a2cd41a1a072849
tetzng <ffffffffffffunk@gmail.com>
2019-11-13 08:11:34 +0900

    Create user.rb

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    name                  {"name"}
+    email                 {"aaaa@gmail.com"}
+    password              {"0000aaaa"}
+    password_confirmation {"0000aaaa"}
+  end
+
+end
