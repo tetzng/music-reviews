c47cd0c1cfd6d0c87b1631dab9ed6c5105a55a50
tetzng <ffffffffffffunk@gmail.com>
2019-11-13 08:40:05 +0900

    Create track.rb

---
+++ spec/factories/track.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :track do
+    spotify_id {"04p5vze2S7VA5YVDa5ELhY"}
+    name {"Hello - Remastered"}
+    track_number {1}
+    duration_ms {202345}
+    preview_url {"https://p.scdn.co/mp3-preview/a193dbe38ab2df343a4fc7174e6fb9f8aae205c0?cid=56a11c58b86746c19d320e65724680b7"}
+  end
+end
