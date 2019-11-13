1e4e20862ee1aad37fb6d3977e09f7ce49bd2068
tetzng <ffffffffffffunk@gmail.com>
2019-11-14 01:02:22 +0900

    Update music_controller_spec.rb

--- spec/controllers/music_controller_spec.rb
+++ spec/controllers/music_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe MusicController, type: :controller do
   describe 'GET #index' do
-    it "populates an array of tracks" do
+    it "populates an array of tracks ordered by spotify_id ASC" do
       tracks = create_list(:track, 3)
       get :index
-
+      expect(assigns(:tracks)).to match(tracks.sort{ |a, b| a.spotify_id <=> b.spotify_id })
     end
     it "renders the :index template" do
       get :index
