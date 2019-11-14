0315b106499211a4e1c8175bd6d5face80b7c4ec
tetzng <ffffffffffffunk@gmail.com>
2019-11-15 02:11:23 +0900

    Create users.js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,17 @@
+ $(function() {
+  $('#user_avatar').css({
+      'display': 'none'
+  }).change(function() {
+      var val = $(this).val();
+      var path = val.replace(/\\/g, '/');
+      var match = path.lastIndexOf('/');
+  $('#user_avatar_name').css("display","inline-block");
+      $('#user_avatar_name').val(match !== -1 ? val.substring(match + 1) : val);
+  });
+  $('#user_avatar_name').bind('keyup, keydown, keypress', function() {
+    return false;
+  });
+  $('#user_avatar_name, #btn').click(function() {
+    $('#user_avatar').trigger('click');
+  });
+});
