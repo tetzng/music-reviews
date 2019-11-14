6814f65b1c11ee2e5ad65e7d1d22ae3b603b14fe
tetzng <ffffffffffffunk@gmail.com>
2019-11-14 21:12:50 +0900

    [Corrected] Layout/SpaceBeforeBlockBraces

--- spec/controllers/music_controller_spec.rb
+++ spec/controllers/music_controller_spec.rb
@@ -5,7 +5,7 @@ describe MusicController, type: :controller do
     it "populates an array of tracks ordered by spotify_id ASC" do
       tracks = create_list(:track, 3)
       get :index
-      expect(assigns(:tracks)).to match(tracks.sort{ |a, b| a.spotify_id <=> b.spotify_id })
+      expect(assigns(:tracks)).to match(tracks.sort { |a, b| a.spotify_id <=> b.spotify_id })
     end
     it "renders the :index template" do
       get :index
