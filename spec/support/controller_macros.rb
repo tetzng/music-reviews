f27dddcb42b093ea462ff684a5d54d15c19b3020
tetzng <ffffffffffffunk@gmail.com>
2019-11-16 04:59:31 +0900

    Update controller_macros.rb

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 module ControllerMacros
   def login(user)
-    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     sign_in user
   end
 end
