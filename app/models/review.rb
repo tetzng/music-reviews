a138157111386bf9505c4dd25f54c4ba45a876c8
tetzng <ffffffffffffunk@gmail.com>
2019-11-16 04:25:40 +0900

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,7 @@
 class Review < ApplicationRecord
   belongs_to :track, foreign_key: 'track_spotify_id', primary_key: 'spotify_id', class_name: 'Track'
   belongs_to :user, optional: true
+
+  validates :rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, allow_blank: true
+  validates :rate, presence: true, unless: [:review?, :user_id?]
 end
