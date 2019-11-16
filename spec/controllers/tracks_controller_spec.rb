0c116b251eb8372bb8de34f89657d1617d671008
tetzng <ffffffffffffunk@gmail.com>
2019-11-17 06:34:36 +0900

    Update tracks_controller_spec.rb

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe TracksController, type: :controller do
+  describe '#index' do
+    context 'with keyword' do
+      before do
+        get :index, params: { keyword: "keyword" }
+      end
 
+      it 'redners index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'without keyword' do
+      before do
+        get :index, params: { keyword: "" }
+      end
+
+      it 'redirects to root_path' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
