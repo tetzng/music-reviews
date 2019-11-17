b8c45fa9b5ae9eb3f922c7c70246ac96e3618c70
tetzng <ffffffffffffunk@gmail.com>
2019-11-17 22:36:30 +0900

    Update albums_controller.rb

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,14 +3,24 @@
 class AlbumsController < ApplicationController
   def index
     @keyword = params[:keyword]
+    @limit = 20
+    @offset = if params[:page] == nil
+              0
+            else
+              (params[:page].to_i - 1) * limit
+            end
+    # "#{@offset + 1}-#{@offset + @limit}件"
     if @keyword.blank?
       redirect_to root_path
     else
-      @albums = RSpotify::Album.search(@keyword)
+      @albums = RSpotify::Album.search(@keyword, limit: @limit, offset: @offset)
     end
   end
 
   def show
     @album = RSpotify::Album.find(params[:spotify_id])
+    # idがないページの場合
+    # rescue RestClient::BadRequest
+    #   redirect_to root_path
   end
 end
