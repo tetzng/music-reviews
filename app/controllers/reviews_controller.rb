6470730fb6e5210a7eefa848dc3406442b1187a5
tetzng <ffffffffffffunk@gmail.com>
2019-11-17 23:12:27 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,9 +19,9 @@ class ReviewsController < ApplicationController
 
   def create_params
     if user_signed_in?
-      params.require(:review).permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id], user_id: current_user.id)
+      params.permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id], user_id: current_user.id)
     else
-      params.require(:review).permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id])
+      params.permit(:rate, :review).merge(track_spotify_id: params[:track_spotify_id])
     end
   end
 end
