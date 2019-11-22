ec267f269a418b520824e6866fce7dba49b202a4
tetzng <ffffffffffffunk@gmail.com>
2019-11-22 15:38:34 +0900

    Update reviews_controller_spec.rb

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -3,20 +3,20 @@
 require 'rails_helper'
 
 describe ReviewsController, type: :controller do
-  let(:track) { create(:track, spotify_id: "3LGRrDmbePCJb4nnkwZSRJ") }
+  let(:track) { create(:track, spotify_id: '3LGRrDmbePCJb4nnkwZSRJ') }
   let(:user) { create(:user) }
 
   describe '#create' do
     context 'can save' do
-      let(:params) { { rate: 2.5, review: "hoge", track_spotify_id: track.spotify_id, user_id: user.id } }
+      let(:params) { { rate: 2.5, review: 'hoge', track_spotify_id: track.spotify_id, user_id: user.id } }
 
-      subject {
+      subject do
         post :create,
-        params: params
-      }
+             params: params
+      end
 
       it 'count up review' do
-        expect{ subject }.to change(Review, :count).by(1)
+        expect { subject }.to change(Review, :count).by(1)
       end
 
       it 'redirects to track_path' do
@@ -28,13 +28,13 @@ describe ReviewsController, type: :controller do
     context 'can not save' do
       let(:invalid_params) { { rate: nil, review: nil, track_spotify_id: track.spotify_id, user_id: nil } }
 
-      subject {
+      subject do
         post :create,
-        params: invalid_params
-      }
+             params: invalid_params
+      end
 
       it 'does not count up' do
-        expect{ subject }.not_to change(Review, :count)
+        expect { subject }.not_to change(Review, :count)
       end
 
       it 'redirects to track_path' do
