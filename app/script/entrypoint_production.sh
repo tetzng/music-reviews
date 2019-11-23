5fa2ba29ac5d2d7fa8aed10f00690ab0616596c3
tetzng <ffffffffffffunk@gmail.com>
2019-11-23 14:26:44 +0900

    Create entrypoint_production.sh

---
+++ app/script/entrypoint_production.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo -------------------------------
+if [ -e /tmp/pids/server.pid ]; then
+    rm -f /tmp/pids/server.pid;
+fi
+
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rails s -p 3000 -b '0.0.0.0'
+echo -------------------------------
