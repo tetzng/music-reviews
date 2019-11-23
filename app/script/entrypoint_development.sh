b0a437bc898ff5bfaeaf01e8517c358e5353a901
tetzng <ffffffffffffunk@gmail.com>
2019-11-23 14:56:34 +0900

    Update entrypoint_development.sh

--- app/script/entrypoint_development.sh
+++ app/script/entrypoint_development.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 echo -------------------------------
-if [ -e /tmp/pids/server.pid ]; then
-    rm -f /tmp/pids/server.pid;
-fi
-
+rm -f tmp/pids/server.pid
 bundle exec rails db:migrate
 bundle exec rails s -p 3000 -b '0.0.0.0'
 echo -------------------------------
