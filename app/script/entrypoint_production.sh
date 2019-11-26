61ac79119fab2d0ff6b2351e71041f39ba10600b
tetzng <ffffffffffffunk@gmail.com>
2019-11-26 09:43:48 +0900

    Update entrypoint_production.sh

--- app/script/entrypoint_production.sh
+++ app/script/entrypoint_production.sh
@@ -4,5 +4,5 @@ rm -f tmp/pids/server.pid
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rake assets:precompile
-bundle exec rails s -p 3000 -b '0.0.0.0'
+bundle exec rails s puma -p 3000 -b '0.0.0.0'
 echo -------------------------------
