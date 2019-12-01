85f0602bf60614a14065e286de728b30eb780da4
tetzng <ffffffffffffunk@gmail.com>
2019-12-01 21:38:57 +0900

    [Corrected] Style/StringLiterals

--- db/migrate/20191108120920_create_artists.rb
+++ db/migrate/20191108120920_create_artists.rb
@@ -2,7 +2,7 @@
 
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
-    create_table :artists, primary_key: :spotify_id, options: "DEFAULT CHARSET=utf8" do |t|
+    create_table :artists, primary_key: :spotify_id, options: 'DEFAULT CHARSET=utf8' do |t|
       t.string :name
 
       t.timestamps
