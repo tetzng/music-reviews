e068b0f63f63bd118160c190c816a0c4d4496e7f
tetzng <ffffffffffffunk@gmail.com>
2019-12-01 19:04:29 +0900

    Update 20191107185749_create_tracks.rb

--- db/migrate/20191107185749_create_tracks.rb
+++ db/migrate/20191107185749_create_tracks.rb
@@ -2,7 +2,7 @@
 
 class CreateTracks < ActiveRecord::Migration[5.2]
   def change
-    create_table :tracks, primary_key: :spotify_id do |t|
+    create_table :tracks, primary_key: :spotify_id, options: "DEFAULT CHARSET=utf8" do |t|
       t.string :name
       t.integer :track_number
       t.integer :duration_ms
