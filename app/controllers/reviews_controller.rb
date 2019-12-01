a4fa25dc46a5d51b5af53b51e6aee00365bb3660
tetzng <ffffffffffffunk@gmail.com>
2019-12-02 05:55:19 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_review, only: %i[edit update destroy]
 
   def create
