a4635e20736c485b8a5fc8dc7be21b2646eaa08c
tetzng <ffffffffffffunk@gmail.com>
2019-12-05 18:53:30 +0900

    Update entrypoint_development.sh

--- app/script/entrypoint_development.sh
+++ app/script/entrypoint_development.sh
@@ -2,5 +2,5 @@
 echo -------------------------------
 rm -f tmp/pids/server.pid
 bundle exec rails db:migrate
-bundle exec rails s -p 3000 -b '0.0.0.0'
+bundle exec puma -C puma.rb
 echo -------------------------------
