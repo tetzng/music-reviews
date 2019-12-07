8c587aa0f3fa78095ae1b79aa1acba4ad33c0588
tetzng <ffffffffffffunk@gmail.com>
2019-12-08 01:58:25 +0900

    Update reviews.js

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -12,8 +12,20 @@ $(document).on('turbolinks:load', function(){
   });
 
   $('#checked-rate input[name="rate"]').on('change', function(){
-    let rateNum = $(this).val();
-    console.log(rateNum);
+    const rateNum = $(this).val();
     $("#rate_view").html(rateNum);
   });
+
+  $('#checked-rate-edit input[name="review[rate]"]').on('change', function(){
+    const rateNum = $(this).val();
+    $("#rate_view").html(rateNum);
+  });
+
+  if(document.URL.match(/reviews/) && document.URL.match(/edit/)) {
+    $(document).ready(function(){
+      const rateValue = $('#checked-rate-edit').data('rate-value');
+      $(`input[value="${rateValue}"]`).prop('checked', true);
+      $("#rate_view").html(rateValue);
+    });
+  }
 });
