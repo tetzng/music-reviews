3deb6e32c9780d273f9e641622316a507309b584
tetzng <ffffffffffffunk@gmail.com>
2019-12-10 03:29:46 +0900

    Update music_controller.rb

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class MusicController < ApplicationController
-  def index; end
+  def index
+    track_spotify_ids = Review.group(:track_spotify_id).average(:rate).sort_by{ |k, v| v.to_f }.reverse.slice(0..4).to_h.keys
+    @ranking = track_spotify_ids.map{ |id| Track.find_by(spotify_id: id) }
+  end
 end
