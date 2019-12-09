1f8d4f9115013357271f7c01283f791e8d3ee459
tetzng <ffffffffffffunk@gmail.com>
2019-12-10 07:56:26 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.includes(:track)
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to :root, alert: 'User not found'
   end
 end
