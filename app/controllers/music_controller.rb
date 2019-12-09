75994bdcdb6473e3b38e2c2d55dc9fb5a85d6595
tetzng <ffffffffffffunk@gmail.com>
2019-12-10 07:02:06 +0900

    Update music_controller.rb

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -2,7 +2,9 @@
 
 class MusicController < ApplicationController
   def index
-    track_spotify_ids = Review.group(:track_spotify_id).average(:rate).sort_by{ |k, v| v.to_f }.reverse.slice(0..4).to_h.keys
+    track_spotify_ids = Review.group(:track_spotify_id, :rate).order('average_rate DESC').limit(5).average(:rate).keys
     @ranking = track_spotify_ids.map{ |id| Track.find_by(spotify_id: id) }
+    review_ids = Review.select(:id).order('id DESC').limit(5)
+    @new_reviews = review_ids.map{ |id| Review.find_by(id: id) }
   end
 end
