af30f4404943ab5b20946ebddf3d90a5bd6b590c
tetzng <ffffffffffffunk@gmail.com>
2019-12-12 18:08:09 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  PER = 3
+  PER = 5
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.includes(:track).page(params[:page]).per(PER)
