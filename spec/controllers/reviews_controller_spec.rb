a11ac1fb5840ef6ad174fc85366f8b207009efa3
tetzng <ffffffffffffunk@gmail.com>
2019-12-13 21:24:57 +0900

    Corrected rubocop lint

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -8,39 +8,39 @@ describe ReviewsController, type: :controller do
 
   describe '#create' do
     context 'can save' do
-  #     let(:params) { { rate: 2.5, review: 'hoge', track_spotify_id: track.spotify_id, user_id: user.id } }
+      #     let(:params) { { rate: 2.5, review: 'hoge', track_spotify_id: track.spotify_id, user_id: user.id } }
 
-  #     subject do
-  #       post :create,
-  #            params: params
-  #     end
+      #     subject do
+      #       post :create,
+      #            params: params
+      #     end
 
-  #     it 'count up review' do
-  #       expect { subject }.to change(Review, :count).by(1)
-  #     end
+      #     it 'count up review' do
+      #       expect { subject }.to change(Review, :count).by(1)
+      #     end
 
-  #     it 'redirects to track_path' do
-  #       subject
-  #       expect(response).to redirect_to(track_path(track.spotify_id))
-  #     end
+      #     it 'redirects to track_path' do
+      #       subject
+      #       expect(response).to redirect_to(track_path(track.spotify_id))
+      #     end
     end
 
     context 'can not save' do
-  #     let(:invalid_params) { { rate: nil, review: nil, track_spotify_id: track.spotify_id, user_id: nil } }
+      #     let(:invalid_params) { { rate: nil, review: nil, track_spotify_id: track.spotify_id, user_id: nil } }
 
-  #     subject do
-  #       post :create,
-  #            params: invalid_params
-  #     end
+      #     subject do
+      #       post :create,
+      #            params: invalid_params
+      #     end
 
-  #     it 'does not count up' do
-  #       expect { subject }.not_to change(Review, :count)
-  #     end
+      #     it 'does not count up' do
+      #       expect { subject }.not_to change(Review, :count)
+      #     end
 
-  #     it 'redirects to track_path' do
-  #       subject
-  #       expect(response).to redirect_to(track_path(track.spotify_id))
-  #     end
+      #     it 'redirects to track_path' do
+      #       subject
+      #       expect(response).to redirect_to(track_path(track.spotify_id))
+      #     end
     end
   end
 end
