8fdc7f3469a2a5153ab5245ee04dd87239de1263
tetzng <ffffffffffffunk@gmail.com>
2019-12-15 15:53:26 +0900

    set env var master_key

--- spec/controllers/music_controller_spec.rb
+++ spec/controllers/music_controller_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 describe MusicController, type: :controller do
   describe 'GET #index' do
-    # it 'renders the :index template' do
-    #   get :index
-    #   expect(response).to render_template :index
-    # end
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 end
